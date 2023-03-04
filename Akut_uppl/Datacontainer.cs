8a6e51d40d51d797e04b7dba020b3dfa8660d925
Tommy Hagberg <tommy.hagberg@vgregion.se>
2023-03-04 10:30:47 +0100

    ny

---
+++ Akut_uppl/Datacontainer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akutl_uppl
+{
+    class Datacontainer
+    {
+        public static string connectionString;
+        public static string anvandarnamn;
+        public static string losen;
+        public static string connectsource;
+        public static string connectstring;
+        public static SqlConnection cnn;
+        public static SqlCommand command,command2;
+        public static SqlDataReader dataReader;
+        public static string personnummer;
+        //  public static string[] personnummerarray;
+        public static List<int> Indexarray = new List<int>();
+        public static List<string> personnummerarray = new List<string>();
+        public static List<string> Familynamearray = new List<string>();
+        public static List<string> Fornamnarray = new List<string>();
+        public static int personnummerindex;
+        public static string tabell;
+        public static string Familyname;
+        public static string fornamn;
+        public static string Signature;
+        public static string Familjenummer;
+        public static string Doctor;
+        public static string Customer_name;
+        public static string Spicemen_type;
+        public static string Indication;
+        public static string Result;
+        public static string Quality;
+        public static int Qualityint;
+        public static int Doctorindex;
+        public static int besvarad;
+        public static string Diagnos;
+        public static string Betalningsansvarig;
+        public static Boolean knappdisable1;
+        public static Boolean knappdisable2;
+        public static int analystyp;
+        public static int fortur;
+        public static int indikation;
+        public static int forskning;
+        public static string analystyp_klartext;
+        public static bool internbetalning;
+        public static bool first_time_price;
+        public static int orderer_internal;
+        public static int betalansvarnummer;
+        public static int Resultat;
+        public static int prenetal;
+        public static int saved_material;
+        public static int Locked;
+        public static int family;
+        public static int orderer;
+        //  public Form1 f1 = new Form1();
+        // public Form2 f2 = new Form2();
+    }
+}
---
+++ Akut_uppl/Form1.cs
@@ -0,0 +1,40 @@
+﻿using Akutl_uppl;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Windows.Forms;
+
+namespace Akut_uppl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Datacontainer.anvandarnamn = "SA.tomha5";
+            //////
+            Datacontainer.losen = "Borg0045!!";
+            //  Datacontainer.connectsource = @"Data Source=Klingen-su-db,62468;Persist Security Info=True;Integrated Security=true; Initial Catalog = Klingen;";
+              Datacontainer.connectsource = @"Data Source=VGDB1472\i01\Klingen_su_db,62468;Persist Security Info=True;Integrated Security=SSPI; Initial Catalog = Klingen;";
+
+            //  VGDB1472\i01\Klingen_su_db,62468
+            Datacontainer.connectionString = @Datacontainer.connectsource + "User ID=" + Datacontainer.anvandarnamn + ";Password=" + Datacontainer.losen + "";
+            Datacontainer.cnn = new SqlConnection(Datacontainer.connectionString);
+            Datacontainer.cnn.Open();
+            MessageBox.Show("Connection Open  !");
+        }
+    }
+
+   
+}
